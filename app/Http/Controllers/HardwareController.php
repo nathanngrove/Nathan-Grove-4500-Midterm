6ff9e75e075adf17b831613f6da7c05b1c3e8e88
Nathan Grove <nathangrove08@gmail.com>
2022-03-24 19:59:26 -0700

    Checking some features for creating hardware and displaying it

--- app/Http/Controllers/HardwareController.php
+++ app/Http/Controllers/HardwareController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hardware;
 
 class HardwareController extends Controller
 {
--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Manufacturer;
 
 class ManufacturerController extends Controller
 {
